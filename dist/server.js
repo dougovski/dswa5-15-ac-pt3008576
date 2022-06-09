1c0d69fdf907b6cbab53535b976c39ea5b84f27a
Douglas <88936801+dougovski@users.noreply.github.com>
2022-06-09 02:14:24 -0300

    Update server.js

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,7 @@
 /*
 var http = require('http');
 var app = require('./config/express')();
-const url = 'mongodb://dswa5:dswa5@cluster0-shard-00-00.aoz22.mongodb.net:27017,cluster0-shard-00-01.aoz22.mongodb.net:27017,cluster0-shard-00-02.aoz22.mongodb.net:27017/ifsp?ssl=true&replicaSet=atlas-12vniy-shard-0&authSource=admin&retryWrites=true&w=majority';
+const url = 'mongodb+srv://dougovski:1409@clusterdhaps.rzyac.mongodb.net/ifsp?retryWrites=true&w=majority';
 require('./config/database.js')(url);
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express Server escutando na porta ' + app.get('port'));
@@ -23,9 +23,9 @@ console.log('Express Server escutando na porta ' + app.get('port'));
 var http = require('http');
 var express = require('express');
 var app = require('./config/express')();
-//const url = 'mongodb://dswa5:dswa5@cluster0-shard-00-00.aoz22.mongodb.net:27017,cluster0-shard-00-01.aoz22.mongodb.net:27017,cluster0-shard-00-02.aoz22.mongodb.net:27017/ifsp?ssl=true&replicaSet=atlas-12vniy-shard-0&authSource=admin&retryWrites=true&w=majority';
+//const url = 'mongodb+srv://dougovski:1409@clusterdhaps.rzyac.mongodb.net/ifsp?retryWrites=true&w=majority';
 require('./config/passport')();
-require('./config/database')('mongodb://dswa5:dswa5@cluster0-shard-00-00.aoz22.mongodb.net:27017,cluster0-shard-00-01.aoz22.mongodb.net:27017,cluster0-shard-00-02.aoz22.mongodb.net:27017/ifsp?ssl=true&replicaSet=atlas-12vniy-shard-0&authSource=admin&retryWrites=true&w=majority');
+require('./config/database')('mongodb+srv://dougovski:1409@clusterdhaps.rzyac.mongodb.net/ifsp?retryWrites=true&w=majority');
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express Server escutando na porta ' + app.get('port'));
 });
